8d4702db21b4487f0bf2bd15149a0e8f41cd8cb1
VictorJS23 <vb329708@gmail.com>
2026-03-16 20:55:57 -0600

    EJ9 solver

---
+++ EJ9/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+10.
+Hacer un programa que determine cual es un año bisiesto
+*/
+
+Console.Write("Verificar si es biciesto: ");
+int year = int.Parse(Console.ReadLine());
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+    Console.WriteLine("si es bisiesto");
+}
+else
+{
+    Console.WriteLine("no es bisiesto");
+}
