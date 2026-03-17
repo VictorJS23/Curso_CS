43cd293ff7f8ea4b0d76b7b5ab3c73b8e74b3f33
VictorJS23 <vb329708@gmail.com>
2026-03-16 20:38:21 -0600

    EJ8 solve

---
+++ EJ8/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+9.
+Calcular binomio al cuadrado (a+b)²
+*/
+
+// Ejemplo (x+3)^2
+
+string formule(int n, string x)
+{
+    return $"({x}^2 + {n + n}{x} + {Math.Pow(n, 2)})";
+}
+
+string x = "x";
+int n = 3;
+
+Console.WriteLine("El resultado de la ecuacion es: " + formule(n, x));
