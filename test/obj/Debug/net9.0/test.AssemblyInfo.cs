b20dc5c8c9d0907a333d00f0ee8a7a26f755f7f4
VictorJS23 <vb329708@gmail.com>
2026-06-09 19:51:04 -0600

    fix

--- test/obj/Debug/net9.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f291c657468340389b5edd7c230af2f4a4681473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd993a2527e4aca0c1e7af430d2ccfbd37cc40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
