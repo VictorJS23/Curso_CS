e0a901c394ce73e1f1be091b34d49c22981586e5
VictorJS23 <vb329708@gmail.com>
2026-06-12 22:40:48 -0600

    EJ15 solve && testing on ArchLinux

---
+++ Exercises/EJ15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Imprime hasta n la sucesion de Fibonacci
+
+int first = 0;
+int second = 1;
+int result = 0;
+
+for (int i = 0; i <= 10; i++)
+{
+    if (i == 0)
+    {
+        Console.WriteLine(first);
+        continue;
+    }
+    if (i == 1)
+    {
+        Console.WriteLine(second);
+        continue;
+    }
+
+    result = first + second;
+    Console.WriteLine(result);
+
+    first = second;
+    second = result;
+}
