3361ab616df1028fedac7844fae498bc83c80ee0
VictorJS23 <vb329708@gmail.com>
2026-06-16 13:04:18 -0600

    EJ16 solve

---
+++ Exercises/EJ16/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Pares de suma igual.
+Dado un arreglo unidimensional de números enteros y un valor objetivo, encuentra todos los pares de elementos en el arreglo cuya suma sea igual al valor objetivo.
+*/
+
+int[] numeros = { 4, 7, 2, 8, 5, 1, 9, 3, 6, 0 };
+int objetivo = 9;
+
+List<string> resultado = new();
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    for (int j = i + 1; j < numeros.Length; j++)
+    {
+        if (numeros[i] + numeros[j] == objetivo)
+        {
+            resultado.Add($"({numeros[i]}, {numeros[j]})");
+        }
+    }
+}
+
+foreach (string item in resultado)
+{
+    Console.WriteLine(item);
+}
